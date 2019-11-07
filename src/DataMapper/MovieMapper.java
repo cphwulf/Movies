0f58773c793e1082101abf4a32909c4570cd191d
Thor <thor@10.14.6>
2019-11-07 12:02:15 +0100

    moviemapper

--- src/DataMapper/MovieMapper.java
+++ src/DataMapper/MovieMapper.java
@@ -1,13 +1,113 @@
 package DataMapper;
+
+import movies.Model.Movies;
+import movies.Util.DBConnector;
+
 import java.sql.*;
+import java.util.ArrayList;
 
-
-/**
- *
- * @author thor
- */
 public class MovieMapper {
-
-
 	
+	public ArrayList<Movies> getMenuItems() {
+		
+		Connection connection = null;
+		ArrayList<Movies> movieList = new ArrayList<>();
+		
+		Statement statement = null;
+		ResultSet resultSet = null;
+		String sqlQuery = "SELECT * FROM movies";
+		
+		try {
+			connection = DBConnector.getConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sqlQuery);
+			
+			while (resultSet.next()) {
+				int movieID = resultSet.getInt("movie_id");
+				String movieTitle = resultSet.getString("movie_title");
+				String director = resultSet.getString("director");
+				int year = resultSet.getInt("year");
+				Movies movie = new Movies(movieID, movieTitle,director , year);
+				
+				movieList.add(movie);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		return movieList;
+	}
+	
+	public Movies getMovieById(int movieID) {
+		
+		Connection connection = null;
+		Movies movie = null;
+		PreparedStatement ps = null;
+		ResultSet resultSet = null;
+		String sqlQuery = "SELECT * FROM movies where movie_id = ?";
+		
+		try {
+			connection = DBConnector.getConnection();
+			ps = connection.prepareStatement(sqlQuery);
+			ps.setInt(1, movieID);
+			resultSet = ps.executeQuery();
+			
+			while (resultSet.next()) {
+				movieID = resultSet.getInt("movie_id");
+				String movieTitle = resultSet.getString("movie_title");
+				String director = resultSet.getString("director");
+				int year = resultSet.getInt("year");
+				movie = new Movies(movieID, movieTitle, director, year);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return movie;
+	}
 }
